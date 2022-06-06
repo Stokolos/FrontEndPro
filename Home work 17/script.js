cab2605cac567aa09325d451c1951c9b2f335967
Pavel <zpcity2014@gmail.com>
2022-06-06 23:36:44 +0300

    H w 17 times

---
+++ Home work 17/script.js
@@ -0,0 +1,41 @@
+const elements = () => {
+    const wrapperClock = document.createElement("div");
+    wrapperClock.classList.add("clock");
+
+    return wrapperClock
+}
+
+const timer = setInterval(() => {
+    const clock = document.querySelector(".clock")
+    clock.textContent = ""
+    tm(times())
+    console.log(times())
+}, 1000)
+
+
+const times = () => {
+    let date = new Date()
+    
+    let hours = date.getHours().toString().padStart(2, "0");
+    let minutes = date.getMinutes().toString().padStart(2, "0");
+    let seconds = date.getSeconds().toString().padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}`
+
+}
+
+const tm = (item) => {  
+    const arr = ["numerHoursFirst", "numerHoursSecond", "colonFirst", "numerMinutesFirst", "numerMinutesSecond", "colonSecond", "numerSecondsFirst", "numerSecondsSecond"];
+    
+    for (const key of item) {
+        const da = document.querySelector(".clock")
+        const d = document.createElement("div")
+        const a = item.indexOf(key)
+        d.classList.add(arr[item.indexOf(key)])
+        d.textContent = key
+        da.append(d)
+    }
+}
+
+document.body.append(elements())
+tm(times())
